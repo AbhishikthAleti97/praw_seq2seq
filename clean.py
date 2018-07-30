0811e2686644cb26788dc621e8278f7ccfeceedf
AbhishikthAleti97 <abhishikth1997@gmail.com>
2018-07-30 19:19:17 +0530

    Update clean.py

--- clean.py
+++ clean.py
@@ -1,9 +1,6 @@
 def clean(data):
-    data = data.replace("motherfucking","").replace("Fuck","Dang").replace("motherfucker","idiot")
-    data = data.replace("cunt"," idiot").replace("bitch","stupid girl").replace("bitchy","bossy")
-    data = data.replace("asshole","idiot").replace("shitting","kidding").replace("masturbation","sleep").replace("masturbating","sleeping").replace(" dick","n idiot").replace("dicks","idiots")
-    data = data.replace("bitches "," sensitive little girls ").replace("bitch","pain").replace("slut"," bad company ")
-    data = data.replace(" hoe","bad influence ").replace(" fucks ", " bonkers ").replace("fuck","shoot")
+    data = data.replace("shitting","kidding") #add more filters here
+    #take in a dictionary, the next time with a key and a value when using OOPS.
     return data
 
 
